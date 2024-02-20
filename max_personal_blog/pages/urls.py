ee38e6b7de5d4ff731be5ba9053c1d4f96611573
MaxIsayev <max.i@list.ru>
2024-02-20 21:50:37 +0200

    generic views and translations

--- max_personal_blog/pages/urls.py
+++ max_personal_blog/pages/urls.py
@@ -5,5 +5,11 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('pages/', views.page_list, name='page_list'),
     path('pages/<int:pk>/', views.page_detail, name='page_detail'),
-    path('pages/<int:pk>/published/', views.page_published, name='page_published'),    
+    path('pages/<int:pk>/published/', views.page_published, name='page_published'),  
+    path('categories/', views.CategoryListView.as_view(), name='category_list'),
+    path('category/<int:pk>/', views.CategoryDetailView.as_view(), name='category_detail'),  
+    path('categories/create/', views.CategoryCreateView.as_view(), name='category_create'),
+    path('category/<int:pk>/edit/', views.CategoryUpdateView.as_view(), name='category_update'),
+    path('category/<int:pk>/delete/', views.CategoryDeleteView.as_view(), name='category_delete'),
 ]
+
--- max_personal_blog/pages/views.py
+++ max_personal_blog/pages/views.py
@@ -2,7 +2,13 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from . import models
 from django.contrib import messages
+from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views import generic
+from django.urls import reverse
+from django.db.models.query import QuerySet
+from typing import Any
 
 # Create your views here.
 def index(request: HttpRequest) -> HttpResponse:
@@ -38,3 +44,69 @@ def page_published(request: HttpRequest, pk:int) -> HttpResponse:
     return redirect(page_list)
 
 dashboard = _('dashboard')
+
+class CategoryListView(generic.ListView):
+    model = models.Category
+    template_name = 'pages/category_list.html'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        queryset = super().get_queryset()
+        if self.request.GET.get('owner'):
+            queryset = queryset.filter(owner__username=self.request.GET.get('owner'))
+        return queryset
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['user_list'] = get_user_model().objects.all().order_by('username')
+        return context
+
+
+class CategoryDetailView(generic.DetailView):
+    model = models.Category
+    template_name = 'pages/category_detail.html'
+
+class CategoryCreateView(LoginRequiredMixin, generic.CreateView):
+    model = models.Category
+    template_name = 'pages/category_create.html'
+    fields = ('name', )
+
+    def get_success_url(self) -> str:
+        messages.success(self.request, _('category created successfully').capitalize())
+        return reverse('category_list')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+    
+class CategoryUpdateView(
+        LoginRequiredMixin, 
+        UserPassesTestMixin, 
+        generic.UpdateView
+    ):
+    model = models.Category
+    template_name = 'pages/category_update.html'
+    fields = ('name', )
+
+    def get_success_url(self) -> str:
+        messages.success(self.request, _('category updated successfully').capitalize())
+        return reverse('category_list')
+
+    def test_func(self) -> bool | None:
+        return self.get_object().owner == self.request.user or self.request.user.is_superuser
+    
+class CategoryDeleteView(
+        LoginRequiredMixin, 
+        UserPassesTestMixin, 
+        generic.DeleteView
+    ):
+    model = models.Category
+    template_name = 'pages/category_delete.html'
+
+    def get_success_url(self) -> str:
+        messages.success(self.request, _('category deleted successfully').capitalize())
+        return reverse('category_list')
+
+    def test_func(self) -> bool | None:
+        return self.get_object().owner == self.request.user or self.request.user.is_superuser
+
+    
